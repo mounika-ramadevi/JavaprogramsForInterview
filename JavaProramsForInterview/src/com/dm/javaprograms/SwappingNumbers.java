a25ad78fcc0d447038159f7007d03b3bc14293be
694886 <694886@LT107050.mshome.net>
2020-09-14 19:00:43 +0530

    initial commit

---
+++ JavaProramsForInterview/src/com/dm/javaprograms/SwappingNumbers.java
@@ -0,0 +1,19 @@
+package com.dm.javaprograms;
+
+import java.util.Scanner;
+
+public class SwappingNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int firstVar = sc.nextInt();
+		int secondVar = sc.nextInt();
+		System.out.println("Before Swapping "+firstVar+ " "+secondVar);
+		//Logic 1
+		int temp = firstVar;
+		firstVar = secondVar;
+		secondVar = temp;
+		System.out.println("After Swapping"+firstVar+ " "+secondVar);
+	}
+
+}
